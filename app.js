5962bde12f22c6b646b24315afe26bc407eb288e
Volha Haiduk <Alisatonks2385@gmail.com>
2022-10-29 00:22:55 +0200

    refactor: rule 3.4 eliminate tags selectors

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
+//Break things down into smaller steps and take each step at a time.i
 
 
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
+addButton.className = 'button';
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -18,32 +19,40 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.className='list'
+    
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.className="input";
     //label
     var label=document.createElement("label");//label
+    label.className = "label";
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.className = "input";
     //button.edit
     var editButton=document.createElement("button");//edit button
+    editButton.className = "button";
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
+    deleteButton.className = "button";
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.className = "remove-img";
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add('task');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add("task");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add("edit");
 
-    deleteButton.className="delete";
+    deleteButton.classList.add("delete");
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -83,7 +92,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
